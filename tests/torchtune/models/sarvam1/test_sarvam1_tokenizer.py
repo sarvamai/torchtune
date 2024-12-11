2c8d85ca1259721299363a60c2de3adbc4c82f94
Rahul A R <rahul@sarvam.ai>
2024-12-12 05:27:36 +0530

    syned all 3 tokenizers (hf, tt, spm)

--- tests/torchtune/models/sarvam1/test_sarvam1_tokenizer.py
+++ tests/torchtune/models/sarvam1/test_sarvam1_tokenizer.py
@@ -1,11 +1,12 @@
+import sentencepiece as spm
 from transformers import AutoTokenizer, LlamaTokenizer
 from torchtune.models.sarvam1 import sarvam1_tokenizer
 from tqdm import tqdm
 
-hf_llama = LlamaTokenizer(vocab_file='/home/rahul_sarvam_ai/nemo_models/original_tokenizer/updated_tokenizer.model', legacy=False)
-tt_tokenizer = sarvam1_tokenizer('/home/rahul_sarvam_ai/nemo_models/original_tokenizer/tokenizer.model')
-
-hf_llama.chat_template = "{% if messages[0]['role'] == 'system' %}{% set loop_messages = messages[1:] %}{% set system_message = messages[0]['content'] %}{% else %}{% set loop_messages = messages %}{% set system_message = false %}{% endif %}\n{% for message in loop_messages %}\n{% if message['role'] not in ['user', 'assistant', 'tool_calls'] %}\n{{ raise_exception('Invalid role: ' + message['role'] + '. Must be user, assistant, or tool_calls.') }}\n{% endif %}\n{% if loop.index0 == 0 and system_message != false %}\n{% set content = '<<SYS>>\n' + system_message + '\n<</SYS>>\n\n' + message['content'] %}\n{% else %}\n{% set content = message['content'] %}\n{% endif %}\n{% if message['role'] == 'user' %}\n{{ bos_token + '[INST] ' + content.strip() + ' [/INST]' }}\n{% elif message['role'] == 'assistant' %}\n{{ ' ' + content.strip() + ' ' + eos_token }}\n{% elif message['role'] == 'tool_calls' %}\n{{ ' [TOOL_CALLS] ' + content.strip() + ' [/TOOL_CALLS] ' }}\n{% endif %}\n{% endfor %}"
+sp_tokenizer = spm.SentencePieceProcessor(model_file='/home/rahul_sarvam_ai/nemo_models/original_tokenizer/tokenizer.model')
+hf_tokenizer = LlamaTokenizer.from_pretrained('sarvam/sarvam-1-nemo-sft')
+# hf_tokenizer = LlamaTokenizer.from_pretrained('sarvam/sarvam-2b-sft', legacy=False, split_special_tokens=True)
+tt_tokenizer = sarvam1_tokenizer(path='/home/rahul_sarvam_ai/nemo_models/original_tokenizer/tokenizer.model')
 
 import datasets
 indic_ds = datasets.load_dataset('sarvam/indic-sft-dataset-sample-sep2024', split='train')
@@ -14,11 +15,19 @@ chat_ds = datasets.load_dataset('sarvam/norobots_sft', split='train')
 
 indic_ds = indic_ds.map(lambda x: {'text': x['query'] + '\n' + x['response']}, remove_columns=indic_ds.column_names)
 en_ds = en_ds.map(lambda x: {'text': x['question'] + '\n' + x['answer']}, remove_columns=en_ds.column_names)
-chat_ds = chat_ds.map(lambda x: {'text': hf_llama.apply_chat_template([x['messages']], tokenize=False)[0]}, remove_columns=chat_ds.column_names)
+chat_ds = chat_ds.map(lambda x: {'text': hf_tokenizer.apply_chat_template([x['messages']], tokenize=False)[0]}, remove_columns=chat_ds.column_names)
 
 ds = datasets.concatenate_datasets([indic_ds, en_ds])
 for d in tqdm(ds):
-    assert tt_tokenizer.encode(d['text'], add_bos=False, add_eos=False) == hf_llama.encode(d['text'], add_special_tokens=False), f"Mismatch for {d['text']}" 
+    sp_output = sp_tokenizer.encode(d['text'])
+    hf_output = hf_tokenizer.encode(d['text'], add_special_tokens=False)
+    tt_output = tt_tokenizer.encode(d['text'])
+    assert sp_output == hf_output, (f"Mismatch for {d['text']}\n{sp_output}\n{hf_output}")
+    assert sp_output == tt_output, (f"Mismatch for {d['text']}\n{sp_output}\n{tt_output}")
 
 for d in tqdm(chat_ds):
-    assert tt_tokenizer.encode(d['text'], add_bos=False, add_eos=False) == hf_llama.encode(d['text'], add_special_tokens=False), f"Mismatch for {d['text']}" 
+    sp_output = sp_tokenizer.encode(d['text'])
+    hf_output = hf_tokenizer.encode(d['text'], add_special_tokens=False)
+    tt_output = tt_tokenizer.encode(d['text'])
+    assert sp_output == hf_output, (f"Mismatch for {d['text']}\n{sp_output}\n{hf_output}")
+    assert sp_output == tt_output, (f"Mismatch for {d['text']}\n{sp_output}\n{tt_output}")
